bb80faa80a91c23e45cde6e75d28d290eb15c9b3
m00nwtchr <m00nwtchr@duck.com>
2024-02-24 02:34:04 +0100

    Basic login component

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000";
 
-export async function fetchProviders(): Promise<string[]> {
-	return axios.get<string[]>(`${API_URL}/auth/oidc`).then(r => r.data);
-}
+export const fetchProviders = async () => {
+	console.log("Fetching providers...");
+	return axios.get<string[]>(`${API_URL}/auth/oidc`).then((r) => r.data);
+};
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,12 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { queryOptions } from "@tanstack/react-query";
-import { useState } from "react";
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 
 import { fetchProviders } from "../api";
 
 const providersQueryOptions = queryOptions({
 	queryKey: ["providers"],
-	queryFn: () => fetchProviders,
+	queryFn: fetchProviders,
 });
 
 export const Route = createFileRoute("/login")({
@@ -16,17 +15,33 @@ export const Route = createFileRoute("/login")({
 });
 
 function LoginComponent() {
-	const [count, setCount] = useState(0);
+	const { data: providers } = useSuspenseQuery(providersQueryOptions);
 
 	return (
 		<>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+					<img
+						className="mx-auto h-10 w-auto"
+						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+						alt="Your Company"
+					/>
+					<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+						Sign in to your account
+					</h2>
+				</div>
+
+				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+					{providers.map((p) => (
+						<button
+							key={p}
+							type="submit"
+							className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						>
+							Sign in with {p}
+						</button>
+					))}
+				</div>
 			</div>
 		</>
 	);
